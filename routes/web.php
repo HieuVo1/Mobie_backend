50dd1107fbfb2edd9ca600b11df36f0a373a6861
HieuVo <hieuvo044@gmail.com>
2021-01-01 16:57:50 +0700

    ht

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('', function() {
-//   return view("admin.login");
-// });
+Route::get('', function() {
+  return view("admin.login");
+});
 
 Route::get('/admin/test', function() {
   return view("admin.login");
