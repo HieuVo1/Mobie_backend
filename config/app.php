e358f3d43060ef6f802c46a31b79862ecfb6330b
HieuVo <hieuvo044@gmail.com>
2021-01-01 17:12:06 +0700

    push

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://cryptic-lake-96358.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL','https://cryptic-lake-96358.herokuapp.com/'),
 
